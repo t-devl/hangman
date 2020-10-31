a6cc8780b4fae021e7528c99f201c67fd2993d7a
X <>
2020-10-31 15:19:07 +0000

    Add Title styled component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 1rem 0;
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title>Hangman</Title>
+    </div>
+  );
 }
 
 export default App;
