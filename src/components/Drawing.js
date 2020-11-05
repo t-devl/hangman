afad7c3351fc12deed0dc20aebb2cca791970123
X <>
2020-11-05 17:50:29 +0000

    Add a drawing effect to each part of the drawing

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -12,30 +12,37 @@ const Base = styled.div`
   left: 0;
   bottom: 0;
   width: 55%;
+  max-width: ${(props) => (props.visible ? "100%" : 0)};
   height: 4px;
   background-color: black;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-width 0.75s ease;
 `;
 
 const Stand = styled.div`
   position: absolute;
+  left: calc(27.5% - 2px);
+  bottom: 0;
   width: 4px;
   height: 100%;
-  left: calc(27.5% - 2px);
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const AcrossBeam = styled.div`
   position: absolute;
-  top: 8px;
+  top: calc(20% - 6px);
   left: calc(27.5% - 2px);
-  width: 20%;
-  height: 4px;
+  width: 4px;
+  height: 20%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
-  transform-origin: bottom;
-  transform: rotate(-45deg);
+  transform-origin: top;
+  transform: rotate(-135deg);
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const TopBar = styled.div`
@@ -44,8 +51,10 @@ const TopBar = styled.div`
   left: calc(27.5% - 2px);
   height: 4px;
   width: 50%;
+  max-width: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-width 0.75s ease;
 `;
 
 const Noose = styled.div`
@@ -54,26 +63,46 @@ const Noose = styled.div`
   left: calc(77.5% - 6px);
   width: 4px;
   height: 20%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const Head = styled.div`
   position: absolute;
+  z-index: -1;
   top: 20%;
   left: calc(77.5% - 20.5px);
   width: 33px;
   height: 33px;
   border-radius: 50%;
+  background-color: white;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  animation: ${(props) =>
+    props.visible ? "drawCircle 0.2s ease forwards" : ""};
 
-  &::after {
-    position: absolute;
-    content: "";
-    width: 33px;
-    height: 33px;
-    border-radius: 50%;
-    box-shadow: 0 0 0px 4px black;
+  @keyframes drawCircle {
+    0% {
+      box-shadow: 20px -20px 0 4px white, -20px -20px 0 4px white,
+        -20px 20px 0 4px white, 20px 20px 0 4px white, 0 0 0px 4px white;
+    }
+    25% {
+      box-shadow: 0px -43px 0 4px white, -20px -20px 0 4px white,
+        -20px 20px 0 4px white, 20px 20px 0 4px white, 0 0 0px 4px black;
+    }
+    50% {
+      box-shadow: 0px -43px 0 4px white, -43px 0px 0 4px white,
+        -20px 20px 0 4px white, 20px 20px 0 4px white, 0 0 0px 4px black;
+    }
+    75% {
+      box-shadow: 0px -43px 0 4px white, -43px 0px 0 4px white,
+        0px 43px 0 4px white, 20px 20px 0 4px white, 0 0 0px 4px black;
+    }
+    100% {
+      box-shadow: 0px -43px 0 4px white, -43px 0px 0 4px white,
+        0px 43px 0 4px white, 43px 20px 0 4px white, 0 0 0px 4px black;
+    }
   }
 `;
 
@@ -83,56 +112,66 @@ const Body = styled.div`
   left: calc(77.5% - 6px);
   width: 4px;
   height: 25%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const LeftArm = styled.div`
   position: absolute;
   left: calc(77.5% - 6px);
-  top: calc(20% + 8px + 8%);
+  top: calc(30% + 34px);
   width: 4px;
   height: 20%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
-  transform-origin: bottom;
-  transform: rotate(-50deg);
+  transform-origin: top;
+  transform: rotate(130deg);
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const RightArm = styled.div`
   position: absolute;
   left: calc(77.5% - 6px);
-  top: calc(20% + 8px + 8%);
+  top: calc(30% + 34px);
   width: 4px;
   height: 20%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
-  transform-origin: bottom;
-  transform: rotate(50deg);
+  transform-origin: top;
+  transform: rotate(-130deg);
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const LeftLeg = styled.div`
   position: absolute;
-  width: 4px;
-  height: 22.5%;
   left: calc(77.5% - 6px);
   top: calc(45% + 34px);
+  width: 4px;
+  height: 22.5%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
   transform-origin: top;
   transform: rotate(40deg);
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 const RightLeg = styled.div`
   position: absolute;
-  width: 4px;
-  height: 22.5%;
   left: calc(77.5% - 6px);
   top: calc(45% + 34px);
+  width: 4px;
+  height: 22.5%;
+  max-height: ${(props) => (props.visible ? "100%" : 0)};
   background-color: black;
   transform-origin: top;
   transform: rotate(-40deg);
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  transition: max-height 0.75s ease;
 `;
 
 export default function Drawing({ livesLeft }) {
